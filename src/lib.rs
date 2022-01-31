c3102df905fe82700e35417be6c47b0739ed6426
ppoliani <ppoliani@gmail.com>
2022-01-31 11:37:30 +0000

    chore(*): add examples

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,34 @@
 //!
 //! `anchor_safe_math` is a collection of helper numeric operation functions that removes the 
 //! verbosity of checking for overflow, underflow and division by zero errors.
+//! 
+//! # Examples
+//!
+//! ```
+//! use anchor_lang::prelude::*;
+//! use safe_math::{SafeMath};
+//! 
+//! #[program]
+//! pub mod example {
+//!   use super::*;
+//! 
+//!   pub fn instruction(ctx: Context<Instruction>, amount: u64) -> ProgramResult {
+//!     let state = &mut ctx.accounts.state;
+//! 
+//!     // You can apply any of the following operations
+//!     state.total_amount = state.total_amount.safe_add(amount)?;
+//!     state.total_amount = state.total_amount.safe_sub(amount)?;
+//!     state.total_amount = state.total_amount.safe_mul(amount)?;
+//!     state.total_amount = state.total_amount.safe_div(amount)?;
+//!     state.total_amount = state.total_amount.safe_pow(8_u32)?;
+//!   }
+//! }
+//! 
+//! #[derive(Accounts)]
+//! pub struct Instruction<'info> {
+//!   ...
+//! }
+//! ```
 use anchor_lang::prelude::*;
 use std::{
   result::Result as StdResult
@@ -19,34 +47,6 @@ pub enum ErrorCode {
 }
 
 /// Defines a set of safe math operations that return a `ProgramError` which is expected in an anchor instruction execution.
-/// 
-/// # Examples
-///
-/// ```
-/// use anchor_lang::prelude::*;
-/// use safe_math::{SafeMath};
-/// 
-/// #[program]
-/// pub mod example {
-///   use super::*;
-/// 
-///   pub fn instruction(ctx: Context<Instruction>, amount: u64) -> ProgramResult {
-///     let state = &mut ctx.accounts.state;
-/// 
-///     // You can apply any of the following operations
-///     state.total_amount = state.total_amount.safe_add(amount)?;
-///     state.total_amount = state.total_amount.safe_sub(amount)?;
-///     state.total_amount = state.total_amount.safe_mul(amount)?;
-///     state.total_amount = state.total_amount.safe_div(amount)?;
-///     state.total_amount = state.total_amount.safe_pow(8_u32)?;
-///   }
-/// }
-/// 
-/// #[derive(Accounts)]
-/// pub struct Instruction<'info> {
-///   ...
-/// }
-/// ```
 pub trait SafeMath {
   type Output;
 
